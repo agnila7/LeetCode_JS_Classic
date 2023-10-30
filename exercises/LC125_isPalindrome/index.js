bfbbd93fba3b1a877f92f397630c43eebcf44a64
Agnila <baruaagnila7@gmail.com>
2023-10-30 12:48:18 -0400

    [com1]mirror image palindrome solve

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,55 @@
-function isPalindrome(s) {}
+//A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters,
+// it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+function isPalindrome(s) {
+  s = s.toLowerCase().replace(/[\W_]/g, "");
+  console.log(s);
+
+  let left = 0;
+  let right = s.length - 1;
+  console.log(s[right]);
+
+  while (left < right) {
+    console.log(left, right);
+    if (s[left] !== s[right]) {
+      console.log(s[left], s[right]);
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+
+// function isPalindrome(s) {
+//     s = s.toLowerCase.replace(/[\W_]/g, "");
+//     console.log(s);
+
+//     let left = 0;
+//     let right = s.length - 1;
+//   //  let left_last = s.length / 2;
+//   //  let right_last = s.length / 2;
+
+//     while(left < right) {
+//         if (s[left] == s[right]) {
+//             return true;
+//         }
+
+//         else
+//             return false;
+
+//     left++;
+//     right--;
+
+//     }
+//     return null;
+
+// }
 
 module.exports = isPalindrome;
