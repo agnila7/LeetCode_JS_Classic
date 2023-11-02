a65abc255db8cbf7b4eea7271dec100b3fc7a8d4
Agnila <baruaagnila7@gmail.com>
2023-11-02 10:38:29 -0400

    [com 5-1]longest palindrome search in one string: pointer expanding around startindex

--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,37 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+  startindex = 0; // this is the starting point of palindrome, we use slice to find out the length of the palindrome
+  maxlength = 1;
+
+  // helping function to expand the pointer for each for loop run,
+  // to find out if the string is palindrome
+  function expandAround(left, right) {
+    // main equation for finding out if the string porion of for loop is the longest palindrome
+    while (left >= 0 && right <= s.length && s[left] == s[right]) {
+      console.log(left, right, s[left], s[right]);
+      currentPalindrome = right - left + 1;
+      console.log(currentPalindrome);
+
+      if (currentPalindrome > maxlength) {
+        maxlength = currentPalindrome;
+        startindex = left; // confused
+        console.log(maxlength, startindex);
+      }
+      left -= 1;
+      right += 1;
+    }
+  }
+
+  // this goes to each letter from start ;to find out if the longest palindrome exist around this startindex
+  for (i = 0; i <= s.length; i++) {
+    console.log(i);
+    expandAround(i - 1, i + 1); //for odd number length palindrome
+    expandAround(i, i + 1);
+  }
+
+  return s.slice(startindex, startindex + maxlength);
+}
 
 module.exports = longestPalindrome;
